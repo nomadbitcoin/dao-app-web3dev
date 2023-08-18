94b6d319bd021f88ad6c00cce5a9cb47337af6f9
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-08-18 16:36:21 -0300

    Airdrop funcionando perfeitamente para todos os holders da NFT

---
+++ scripts/7-airdrop-token.js
@@ -0,0 +1,42 @@
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    // Esse é o endereço do nosso contrato ERC-1155 do NFT de filiação.
+    const editionDrop = await sdk.getContract("0x5f281928B01F85e47bD5de6fa6FD47fdf3d31d66", "edition-drop");
+    // Esse é o endereço do nosso contrato ERC-20 do nosso token.
+    const token = await sdk.getContract("0x13DD0AA8cE5b77c8BA6cc81d21Ab31F93eD56081","token");
+    // Pegue o endereço de todas as pessoas que possuem o nosso NFT de filiação, que tem
+    // o tokenId 0.
+    const walletAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+  
+    if (walletAddresses.length === 0) {
+      console.log(
+        "Ninguém mintou o NFT ainda, peça para alguns amigos fazerem isso e ganhar um NFT de graça!",
+      );
+      process.exit(0);
+    }
+    
+    // faça um loop no array de endereços.
+    const airdropTargets = walletAddresses.map((address) => {
+      // Escolha um # aleatório entre 1000 e 10000.
+      const randomAmount = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+      console.log("✅ Vai enviar", randomAmount, "tokens para ", address);
+      
+      // Configure o alvo.
+      const airdropTarget = {
+        toAddress: address,
+        amount: randomAmount,
+      };
+  
+      return airdropTarget;
+    });
+    
+    // Chame transferBatch em todos os alvos do airdrop.
+    console.log("🌈 Começando o airdrop...")
+    await token.transferBatch(airdropTargets);
+    console.log("✅ Feito o airdrop de tokens para todos os donos de NFT!");
+  } catch (err) {
+    console.error("O airdrop de tokens falhou", err);
+  }
+})();
