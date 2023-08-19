1d057e1394fd05daad690bedca20d8c97d44f0e1
Yan Luiz Coito <nomadbitcoin@192.168.1.12>
2023-08-18 21:38:00 -0300

    Dashboard de membros sendo mostrado com saldos dos holders

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import { useState, useEffect, useMemo } from 'react';
 const App = () => {
   // Use o hook connectWallet que o thirdweb nos dá.
   const address = useAddress();
-  console.log("👋 Address:", address);
+  // console.log("👋 Address:", address);
   // inicializar o contrato editionDrop
   const editionDropAddress = "0x5f281928b01f85e47bd5de6fa6fd47fdf3d31d66"
   const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  const { contract: token } = useContract('0x13DD0AA8cE5b77c8BA6cc81d21Ab31F93eD56081', 'token');
 
   // Hook para verificar se o úsuario tem a NFT
   const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
@@ -15,6 +16,70 @@ const App = () => {
   const hasClaimedNFT = useMemo(() => {
     return nftBalance && nftBalance.gt(0)
     }, [nftBalance])
+
+  // Guarda a quantidade de tokens que cada membro tem nessa variável de estado.
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+  // O array guardando todos os endereços dos nosso membros.
+  const [memberAddresses, setMemberAddresses] = useState([]);
+  // Uma função para diminuir o endereço da carteira de alguém, não é necessário mostrar a coisa toda.
+  const shortenAddress = (str) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  };
+
+  // Esse useEffect pega todos os endereços dos nosso membros detendo nosso NFT.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+    
+    // Do mesmo jeito que fizemos no arquivo 7-airdrop-token.js! Pegue os usuários que tem nosso NFT
+    // com o tokenId 0.
+    const getAllAddresses = async () => {
+      try {
+        const memberAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+        setMemberAddresses(memberAddresses);
+        console.log("🚀 Endereços de membros", memberAddresses);
+      } catch (error) {
+        console.error("falha ao pegar lista de membros", error);
+      }
+    };
+    getAllAddresses();
+  }, [hasClaimedNFT, editionDrop]);
+
+  // Esse useEffect pega o # de tokens que cada membro tem.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Pega todos os saldos.
+    const getAllBalances = async () => {
+      try {
+        const amounts = await token.history.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log("👜 Quantidades", amounts);
+      } catch (error) {
+        console.error("falha ao buscar o saldo dos membros", error);
+      }
+    };
+    getAllBalances();
+  }, [hasClaimedNFT, token]);
+
+  // Agora, nós combinamos os memberAddresses e os memberTokenAmounts em um único array
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      // Se o endereço não está no memberTokenAmounts, isso significa que eles não
+      // detêm nada do nosso token.
+      const member = memberTokenAmounts?.find(({ holder }) => holder === address);
+
+      return {
+        address,
+        tokenAmount: member?.balance.displayValue || "0",
+      }
+    });
+  }, [memberAddresses, memberTokenAmounts]);
+
+  console.log("memberList: ", memberList)
 
   // Esse é o caso em que o usuário ainda não conectou sua carteira
   // ao nosso webapp. Deixe ele chamar connectWallet.
@@ -29,14 +94,39 @@ const App = () => {
     );
   }
 
+  // Se o usuário já reivindicou seu NFT nós queremos mostrar a página interna da DAO para ele
+  // Apenas membros da DAO vão ver isso. Renderize todos os membros + quantidade de tokens
   if (hasClaimedNFT) {
     return (
       <div className="member-page">
-        <h1> Bem vindo à MultiplicaDAO</h1>
-        <p>Parabéns por fazer parte dos membros Multiplicadores!</p>
+        <h1>Página dos membros da DAO</h1>
+        <p>Parabéns por fazer parte desse grupo de multiplicadores</p>
+        <div>
+          <div>
+            <h2>Lista de Membros</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Endereço</th>
+                  <th>Quantidade de Tokens</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-    )
-   };
+    );
+  };
   
   // Renderiza a tela de cunhagem do NFT.
   return (
